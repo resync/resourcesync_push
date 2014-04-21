58fa8962bbe803b951da9151c295e4ad46b7de20
Harihar Shankar <hariharshankar@gmail.com>
2014-04-21 09:16:24 -0600

    updated doc, fixed manifest and setup

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ except ImportError:
 
 
 setup(name='resourcesync_push',
-      version='0.0.3',
+      version='0.3.0',
       author='Harihar Shankar, Martin Klien, Herbert Van de Sompel',
       author_email="hariharshankar@gmail.com",
-      url="",
+      url="http://resourcesync.org",
       download_url="",
-      description="",
+      description="A ResourceSync PubSubHubhub implementation.",
       long_description="",
       packages=[
           'resourcesync_push',
@@ -18,8 +18,8 @@ setup(name='resourcesync_push',
           'resourcesync_push.publisher',
           'resourcesync_push.subscriber'
       ],
-      keywords="",
-      license='',
+      keywords="resourcesync pubsubhubbub uwsgi python",
+      license='http://mementoweb.github.io/SiteStory/license.html',
       install_requires=[
           "uWSGI>=2.0.3",
           "requests>=2.2.1",
@@ -28,7 +28,7 @@ setup(name='resourcesync_push',
       tests_require=[
           'WebTest>=2.0.14'
       ],
-      test_suite='resourcesync_push.test',
+      test_suite='test',
       scripts=['bin/resourcesync_hub',
                'bin/resourcesync_sub',
                'bin/resourcesync_pub'],
